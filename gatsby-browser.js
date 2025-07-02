11e8865cdaa9b38cef0cd3aa1dce9a59a9a3b41e
Mark Austin <mark.vb.austin@gmail.com>
2025-07-02 12:26:22 +0100

    build(gatsby): globally add YARL styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,6 @@ import "typeface-merriweather"
 import "typeface-nunito"
 import "typeface-alegreya"
 import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
