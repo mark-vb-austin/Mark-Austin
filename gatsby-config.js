0c2e87b64fc6e42b79e0d727d2460cbcb0361700
Mark Austin <mark.vb.austin@gmail.com>
2025-07-02 12:08:19 +0100

    build(gatsby): whitelist yarl from purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,6 +108,9 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
+        safelist: [
+          /^yarl/, // This will keep all classes that start with 'yarl'
+        ],
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
