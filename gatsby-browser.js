c84cbaf19d86d3b69a1e388ba88d68d970352457
Mark Austin <mark.vb.austin@gmail.com>
2025-07-02 23:03:57 +0100

    ah-chooo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,5 @@ import "typeface-merriweather"
 import "typeface-nunito"
 import "typeface-alegreya"
 import "bootstrap/dist/css/bootstrap-grid.min.css";
-import "yet-another-react-lightbox/dist/styles.css";
-import "yet-another-react-lightbox/dist/plugins/thumbnails/thumbnails.css";
+// import "yet-another-react-lightbox/dist/styles.css";
+// import "yet-another-react-lightbox/dist/plugins/thumbnails/thumbnails.css";
