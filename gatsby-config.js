c3412df91705db578059ea75daf08d729252c6e1
Mark Austin <mark.vb.austin@gmail.com>
2025-07-02 14:43:32 +0100

    test(purgecss): comment out purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,21 +104,16 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        safelist: [
-          /^yarl/, // all yarl classes
-          /yet-another-react-lightbox/, // sometimes the package name is used in classnames
-        ],
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-purgecss`,
+    //   options: {
+    //     printRejected: true, // Print removed selectors and processed file names
+    //     safelist: [
+    //       /^yarl/, // all yarl classes
+    //       /yet-another-react-lightbox/, // sometimes the package name is used in classnames
+    //     ],
+    //   },
+    // },
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
